3ea4f3e071bd21a55672b4c5b217aad6f95008d4
adriancobon <adriancobon@outlook.com>
2019-08-26 15:34:20 -0400

    First commit

---
+++ src/components/dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "shards-react";
+import Menu from "./menu";
+
+export default function Dashboard() {
+    return (
+        <Container className="DashboardContainer">
+            // This section needs to pull in the exercise className
+
+            // This section needs to create a slider for inputting the new entry
+
+            // This section should tally the daily average
+            
+        </Container>
+    )
+}
