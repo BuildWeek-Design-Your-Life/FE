f107aad9fad9785ef47cd97ce239f1d0dafbc483
adriancobon <adriancobon@outlook.com>
2019-08-26 16:41:26 -0400

    Added notes and some preliminary code.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -14,18 +14,21 @@ export default function Dashboard() {
                 <div>
                     <H1>Activity Name:</H1>
                         // Code to pull activity name from database
+                        let activity = axios.get(username.activitynumber);
+                        console.log activity
                 </div>
             // This section needs to create a slider for inputting the new entry
                 <Slider connect={[true, false]} start={[1]} range={{ min: 0, max: 5 }} />
             // This section should tally the daily average
                 // This section will get the previous total
-
+                   let total = axios.get(username.activitynumber.total);
                 // This section will get the total number of days and add 1. Finally saving it.
                     // days = days + 1
-                // This section will add the new total to the previous total and save inputting
-                    // newTotal = oldTotal + Slider
+                    let days = axios.get(username.activitynumber.days) + 1; 
+                // This section will add the new total to the previous total and save the input
+                    // let newTotal = total + Slider
                 // This section will show the results of newtotal/totalnumber of days.
-
+                    let average = newTotal / days;
         </Container>
     )
 }
