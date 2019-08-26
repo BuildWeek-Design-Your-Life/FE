629a7ace85544a7310ec34c4987fd7ee4986a7f1
adriancobon <adriancobon@outlook.com>
2019-08-26 15:54:11 -0400

    Added notes in dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "shards-react";
+import { Slider } from "shards-react";
 import Menu from "../components/menu";
 import "../index.css";
 
@@ -9,9 +9,16 @@ export default function Dashboard() {
             // This section needs to pull in the exercise className
 
             // This section needs to create a slider for inputting the new entry
-
+                <Slider connect={[true, false]} start={[1]} range={{ min: 0, max: 5 }} />
             // This section should tally the daily average
+                // This section will get the previous total
 
+                // This section will get the total number of days and add 1. Finally saving it.
+
+                // This section will add the new total to the previous total and save inputting
+
+                // This section will show the results of newtotal/totalnumber of days.
+                
         </Container>
     )
 }
