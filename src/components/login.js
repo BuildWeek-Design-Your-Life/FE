7ecefdeaac8df7d56c907815df2daf7ed94093a3
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-26 16:22:03 -0400

    Need a code review. Did my best to apply the concepts from pasted guided projects and such, but am not 100% sure how to revise the syntax so it will work with this particular app and framework.

--- src/components/login.js
+++ src/components/login.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormInput, FormGroup, Button, Container } from "shards-react";
+import axios from "axios";
 
-export default function Login() {
+export default function Login () {
+  const [user, setUser] = useState({username: "", password: ""});
+
+  const changeHandler = event => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setUser({...user, [event.target.name]: event.target.value})
+  }
+
+  const submitForm = event => {
+    event.preventDefault();
+    axios
+      .post("https://reqres.in/api/users")
+      .then(res => {
+        setUser({username: "", password: ""});
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
     <Container className="login-container">
-      <Form>
-        <FormGroup>
+      <Form validated="true" onSubmit={submitForm}>
+        <FormGroup required>
           <label htmlFor="#username">Username</label>
-          <FormInput id="#username" placeholder="Username" />
+          <FormInput id="#username" name="username" placeholder="Username" value={user.username} onChange={changeHandler} />
         </FormGroup>
-        <FormGroup>
+        <FormGroup required>
           <label htmlFor="#password">Password</label>
-          <FormInput type="password" id="#password" placeholder="Password" />
+          <FormInput type="password" name="password" id="#password" placeholder="Password" value={user.password} onChange={changeHandler} invalid-feedback="Password is invalid. Try again!" />
         </FormGroup>
         <Button>Login</Button>
       </Form>
     </Container>
-  );
+  )
 }
