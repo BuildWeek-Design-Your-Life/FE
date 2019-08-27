9126442b32e84522af62887c922f16d588b13725
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-27 15:47:11 -0400

    Added card with Shards syntax.

--- src/components/activities/ActivityCard.js
+++ src/components/activities/ActivityCard.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-import { Container, Row } from "shards-react";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Container, 
+    Row
+  } from "shards-react";
 
 export default function ActivityCard(props) {
   return (
     <Container>
-      <Row>
-        THIS IS WHERE THE ACTIVITY CARD GOES
+        <Row>
+        <Card style={{ maxWidth: "300px" }}>
+            <CardHeader>Activity Name</CardHeader>
+            <CardBody>
+                <p>Energy: </p>
+                <p>Engagement: </p>
+            </CardBody>
+        </Card>
+
         {/* <h2>{props.activity.activity}</h2> 
                 <p>Energy: {props.activity.energize}</p>
                 <p>Engagement: {props.activity.engagement}</p> */}
