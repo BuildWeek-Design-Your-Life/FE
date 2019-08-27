3f6f6072760d4b60d82239898a6a39ce0a27fbc5
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-27 14:18:10 -0700

    fixed merge errors
    fixed merge errors

--- src/components/activities/ActivityCard.js
+++ src/components/activities/ActivityCard.js
@@ -1,39 +1,33 @@
 import React from "react";
 
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    Container, 
-    Row
-  } from "shards-react";
+import { Card, CardHeader, CardBody, Container, Row } from "shards-react";
 
-  import "../../App.css";
+import "../../App.css";
 
 export default function ActivityCard(props) {
   return (
-
     <Container className="card-container">
-        <Card className="card">
-            <CardHeader className="card-header">Activity Name</CardHeader>
-            <CardBody className="card-body">
-                <p>Energy: </p>
-                <p>Engagement: </p>
-            </CardBody>
-        </Card>
-        <Card className="card">
-            <CardHeader className="card-header">Activity Name</CardHeader>
-            <CardBody className="card-body">
-                <p>Energy: </p>
-                <p>Engagement: </p>
-            </CardBody>
-        </Card>
-        <Card className="card">
-            <CardHeader className="card-header">Activity Name</CardHeader>
-            <CardBody className="card-body">
-                <p>Energy: </p>
-                <p>Engagement: </p>
-            </CardBody>
-        </Card>
+      <Card className="card">
+        <CardHeader className="card-header">Activity Name</CardHeader>
+        <CardBody className="card-body">
+          <p>Energy: </p>
+          <p>Engagement: </p>
+        </CardBody>
+      </Card>
+      <Card className="card">
+        <CardHeader className="card-header">Activity Name</CardHeader>
+        <CardBody className="card-body">
+          <p>Energy: </p>
+          <p>Engagement: </p>
+        </CardBody>
+      </Card>
+      <Card className="card">
+        <CardHeader className="card-header">Activity Name</CardHeader>
+        <CardBody className="card-body">
+          <p>Energy: </p>
+          <p>Engagement: </p>
+        </CardBody>
+      </Card>
+    </Container>
   );
 }
