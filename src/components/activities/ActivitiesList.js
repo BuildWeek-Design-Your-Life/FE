b66dfd7b84679dc320c5f3b5be6129c0afc05e76
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-27 14:25:21 -0700

    commit
    commit cause github

--- src/components/activities/ActivitiesList.js
+++ src/components/activities/ActivitiesList.js
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 import ActivityCard from "./ActivityCard";
-import { axiosWithAuth }  from "../../Utils/axiosWithAuth";
+import { axiosWithAuth } from "../../Utils/axiosWithAuth";
 // import axios from 'axios'
 
 const ActivitiesList = () => {
-    const [activitiesArray, setActivities] = useState([]);
-    console.log(activitiesArray)
-    const getActivities = () => {
-        return axiosWithAuth()
-            .get("https://design-bw.herokuapp.com/api/activity")
-            .then(res => {
-                setActivities(res.data);
-            })
-            .catch(err => {
-                console.log('This is bad', err.response);
-            }) 
-    }
+  const [activitiesArray, setActivities] = useState([]);
+  console.log(activitiesArray);
+  const getActivities = () => {
+    return axiosWithAuth()
+      .get("https://design-bw.herokuapp.com/api/activity")
+      .then(res => {
+        setActivities(res.data);
+      })
+      .catch(err => {
+        console.log("This is bad", err.response);
+      });
+  };
 
-    useEffect(() => {
-        getActivities();
-    }, []) 
-    
-    return (
-        <div className="activities-list grid-view">
-        {activitiesArray.map(active => 
-        <ActivityCard key = {active.id} {...activitiesArray} active={active} />)}
-        </div>
-    )
-}
+  useEffect(() => {
+    getActivities();
+  }, []);
+
+  return (
+    <div className="activities-list grid-view">
+      Thanks GitHub
+      {activitiesArray.map(active => (
+        <ActivityCard key={active.id} {...activitiesArray} active={active} />
+      ))}
+    </div>
+  );
+};
 
 export default ActivitiesList;
