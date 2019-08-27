3bc291d3c4fe272357db95164384fb9a1b76d40a
adriancobon <adriancobon@outlook.com>
2019-08-27 13:47:31 -0400

    Eliminated old material in dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -25,28 +25,5 @@ export default function Dashboard() {
                     console.log('Error fetching and parsing data', error);
                 })
             }
-
-
-        <Container className="DashboardContainer">
-            {/* // This section needs to pull in the exercise className */}
-                <div>
-                    <h1>Activity Name:</h1>
-                        {/* // Code to pull activity name from database
-                        let activity = axios.get(username.activitynumber);
-                        console.log activity */}
-                </div>
-            {/* // This section needs to create a slider for inputting the new entry */}
-                <Slider connect={[true, false]} start={[1]} range={{ min: 0, max: 5 }} />
-            {/* // This section should tally the daily average
-                // This section will get the previous total
-                   let total = axios.get(username.activitynumber.total);
-                // This section will get the total number of days and add 1. Finally saving it.
-                    // days = days + 1
-                    let days = axios.get(username.activitynumber.days) + 1; 
-                // This section will add the new total to the previous total and save the input
-                    // let newTotal = total + Slider
-                // This section will show the results of newtotal/totalnumber of days.
-                    let average = newTotal / days; */}
-        </Container>
     )
 }
