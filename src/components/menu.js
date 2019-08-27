68737e28275a4b3d874d1607137ecaa58d136f2d
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-27 11:19:16 -0700

    edited dashboard
    edited dashboard

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,25 +1,4 @@
-// import React from "react";
-// import { Nav, NavItem, NavLink } from "shards-react";
-
-// export default function Menu() {
-//   return (
-//     <Nav className="nav-container">
-//       <NavItem>
-//         <NavLink active href="#">
-//           Home
-//         </NavLink>
-//       </NavItem>
-//       <NavItem>
-//         <NavLink href="#">Dashboard</NavLink>
-//       </NavItem>
-//       <NavItem>
-//         <NavLink href="#">Sign In</NavLink>
-//       </NavItem>
-//     </Nav>
-//   );
 import React from "react";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Navbar,
   NavbarToggler,
--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -1,58 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class Registration extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email:'',
-            password: '',
-            password_comfirmation:'',
-            registrationErrors:''
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-handleChange(event){
-    console.log("handle change",event)
-}
-handleSubmit(event) {
-    console.log("Submitted")
+    this.state = {
+      email: "",
+      password: "",
+      password_comfirmation: "",
+      registrationErrors: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    console.log("handle change", event);
+  }
+  handleSubmit(event) {
+    console.log("Submitted");
     event.preventDefault();
-}
+  }
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}
-                <input 
-                type="email" 
-                name="email"
-                placeholder="Email"
-                value={this.state.email}
-                onChange{this.handleChange}
-                required
-                />
-                 <form onSubmit={this.handleSubmit}
-                <input 
-                type="password" 
-                name="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange{this.handleChange}
-                required
-                />
-   <form onSubmit={this.handleSubmit}
-                <input 
-                type="password" 
-                name="password_confirmation"
-                placeholder="Password"
-                value={this.state.password_comfirmation}
-                onChange{this.handleChange}
-                required
-                />
-                <button type="submit">Register</button>
-                </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
