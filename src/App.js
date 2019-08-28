9d14093b0daeba1834a04659f72725c9b7a1b696
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-28 14:21:21 -0400

    Need to style, but gotta get AddActivity working first. Out of ideas???

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import Footer from "./components/footer";
 import Login from "./components/login";
 import Menu from "./components/menu";
 import ActivitiesList from "./components/activities/ActivitiesList";
-import ActivityCard from "./components/activities/ActivityCard";
+import ReflectionsList from "./components/relfections/ReflectionsList";
 import Registration from "./components/registration/Registration";
 import Dashboard from "./components/dashboard";
+import AddActivity from "./components/activities/AddActivity";
 
 import { Route, Switch } from "react-router-dom";
 import UpdateForm from "./components/activities/UpdateForm";
 import PrivateRoute from "./components/PrivateRoute";
-
-import ReflectionsList from "./components/relfections/ReflectionsList";
 
 export default class App extends Component {
   render() {
@@ -27,7 +26,7 @@ export default class App extends Component {
         <PrivateRoute exact path="/reflection" component={ReflectionsList} />
         <Route exact path="/register" component={Registration} />
         <Route path="/update-activity/:id" component={UpdateForm} />
-
+        <PrivateRoute exact path='/add-activity' component={AddActivity} />
         <Footer />
       </div>
     );
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import {Button} from "shards-react";
+import axios from "axios";
 import ActivitiesList from "./activities/ActivitiesList";
 import ReflectionsList from "./relfections/ReflectionsList";
 
 export default function Dashboard () {
+  const [inputs, setInputs] = useState([]);
+
   return (
     <>
       <ActivitiesList />
       <ReflectionsList />
+      <Link to={'/add-activity'}>
+        <Button block outline rounded theme="success">
+          Add Activity
+        </Button>
+      </Link>
     </>
   )
 }
