925b493fdb443051a82840a9483bcc3951b8fd2a
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-28 16:32:11 -0400

    Tried to refactor AddActivity in the syntax we were taught, but still not working. Think this approach is lacking information we need. Taking a break and then will go to Help and beg for an assist.

--- src/components/activities/AddActivity.js
+++ src/components/activities/AddActivity.js
@@ -1,59 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {Form, Field, Formik} from 'formik';
 import axiosWithAuth from "../../Utils/axiosWithAuth";
 
-class AddActivity extends Component {
-    state = {
-        newActivity: {
-            Activity: '',
-            Energy: '',
-            Engagement: '',
-            users_ref_id: 1
-        }
-    }
+export default function AddActivity (props) {
 
-    handleChange = e => {
-        this.setState({
-            newActivity: {
-                ...this.state.newActivity,
-                [e.target.name]: e.target.value
-            }
-        })
-        console.log(e.target.value);
-    }
+    const [activity, setActivity] = useState({ Activity: "", Energy: 0, Engagement: 0, ID: ""});
 
-    handleNum = e => {
-        this.setState({
-            newActivity: {
-                ...this.state.newActivity,
-                [e.target.name]: Number(e.target.value)
-            }
-        })
-        console.log(e.target.value);
-    }
+    const handleChange = e => {
+        setActivity({activity, [e.target.name]: e.target.value });
+    };
 
-    onSubmit = e => {
-        e.preventDefault()
-        console.log(this.state.newActivity)
+    const submitForm = e => {
         axiosWithAuth()
-        .post(`https://design-bw.herokuapp.com/api/activity`, this.state.newActivity)
-        .then(res => {console.log(res)})
-        .catch(err => {console.log(err.message)})
+            .post(`https://design-bw.herokuapp.com/api/activity/`, this.state.activity)
+            .then(res => {console.log(res)})
+            .catch(err => {console.log(err.message)})
+            // console.log(res)
     }
 
-    render() {
-        return (
-            <>
-            <Formik>
-                <Form onSubmit={this.onSubmit}>
-                    <Field type="text" name="Activity" value={this.state.activity} onChange={this.handleChange}/>              
-                    <Field type="number" name="Energy" value={this.state.energize} onChange={this.handleNum}/>
-                    <Field type="number" name="Engagement" value={this.state.engagement} onChange={this.handleNum}/>
-                    <button type="submit">Submit</button>
-                </Form>  
-            </Formik>
-            </>
-        )
-    }
+    return (
+        <>
+        <Formik>
+            <Form onSubmit={submitForm}>
+                <Field type="text" name="Activity" onChange={handleChange}/>              
+                <Field type="number" name="Energy" onChange={handleChange}/>
+                <Field type="number" name="Engagement" onChange={handleChange}/>
+                 <button type="submit">Submit</button>
+            </Form>  
+        </Formik>
+        </>
+    )
 }
-export default AddActivity
+
+// class AddActivity extends Component {
+//     state = {
+//         newActivity: {
+//             Activity: '',
+//             Energy: 0,
+//             Engagement: 0,
+//             users_act_id: 1,
+//         }
+//     }
+
+//     handleChange = e => {
+//         this.setState({
+//             newActivity: {
+//                 ...this.state.newActivity,
+//                 [e.target.name]: e.target.value
+//             }
+//         })
+//         console.log(e.target.value);
+//     }
+
+//     handleNum = e => {
+//         this.setState({
+//             newActivity: {
+//                 ...this.state.newActivity,
+//                 [e.target.name]: e.target.value
+//             }
+//         })
+//         console.log(e.target.value);
+//     }
+
+//     onSubmit = e => {
+//         e.preventDefault()
+//         console.log(this.state.newActivity)
+//         axiosWithAuth()
+//         .post(`https://design-bw.herokuapp.com/api/activity/`, this.state.newActivity)
+//         .then(res => {console.log(res)})
+//         .catch(err => {console.log(err.message)})
+//     }
+
+//     render() {
+//         return (
+//             <>
+//             <Formik>
+//                 <Form onSubmit={this.onSubmit}>
+//                     <Field type="text" name="Activity" value={this.state.activity} onChange={this.handleChange}/>              
+//                     <Field type="number" name="Energy" value={this.state.energize} onChange={this.handleNum}/>
+//                     <Field type="number" name="Engagement" value={this.state.engagement} onChange={this.handleNum}/>
+//                     <button type="submit">Submit</button>
+//                 </Form>  
+//             </Formik>
+//             </>
+//         )
+//     }
+// }
+// export default AddActivity
